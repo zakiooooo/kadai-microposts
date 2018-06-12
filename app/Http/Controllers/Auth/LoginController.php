8d4ca81970fbe7f5e88e0eeff21f2d73febe81f7
jiro.kirameki <ec2-user@ip-172-31-35-132.us-east-2.compute.internal>
2018-06-12 04:25:01 +0000

    kaisei

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,21 +25,8 @@ class LoginController extends Controller
      *
      * @var string
      */
-    public function login($id)
-    {
-        $user = \Auth::user();
-        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
-
-        $data = [
-            'user' => $user,
-            'microposts' => $microposts,
-        ];
-
-        $data += $this->counts($user);
-
-        return view('users.show', $data);
-    }
-    //protected $redirectTo = '/';
+    
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
